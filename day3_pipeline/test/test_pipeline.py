44292f8d6f5278da906c60771f3df88537b3da38
gunn <gunnkalra10@gmail.com>
2026-06-05 09:50:04 +0530

    git commit3

--- day3_pipeline/test/test_pipeline.py
+++ day3_pipeline/test/test_pipeline.py
@@ -1,4 +1,8 @@
-from day3_pipeline.data_loader import load_data
+from data_loader import load_data
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 def test_load_data():
